16b21d40e7f69d6684836aa4e3a92ba57e91f986
Mary West <westmarye1219@gmail.com>
2019-03-25 21:10:01 -0500

    buttons are showing unique ids

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const addButton = document.getElementById('addButton');
 const inputIngredient = document.getElementById('inputIngredient');
 
 const ingredients = [];
+let ingredientCounter = 1;
 
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
@@ -14,7 +15,7 @@ const domStringBuilder = (arrayToPrint) => {
       domString += `<div class="card col-3">`;
       domString += `  <div class="card-body">`;
       domString += `    <h5 class="card-title">${ingredient.item}</h5>`;
-    //   domString += `    <a href="#" class="btn btn-primary">Go somewhere</a>`;
+      domString += `    <a class="btn btn-danger deleteButton" id =${ingredient.id}>Delete</a>`;
       domString += `  </div>`;
       domString += `</div>`;
     });
@@ -26,9 +27,11 @@ const addIngredient = (e) => {
     e.preventDefault();
     const inputText = inputIngredient.value;
     const newIngredient = {
-        item: inputText
+        item: inputText,
+        id: `ingredient${ingredientCounter}`,
     };
     ingredients.push(newIngredient);
+    ingredientCounter ++;
     domStringBuilder(ingredients);
     inputIngredient.value = '';
 }
